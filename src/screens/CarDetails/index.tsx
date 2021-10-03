43ef0c6a52df9f717958b9224590990031ec8eb7
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-10-03 12:50:04 -0300

    Schedule

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
 
 import speedSvg from '../../assets/speed.svg';
 import accelerationSvg from '../../assets/acceleration.svg';
@@ -25,6 +26,7 @@ import {
    Price,
    About,
    Accessories,
+   Footer,
 } from './styles';
 
 export function CarDetails(){
@@ -68,6 +70,10 @@ export function CarDetails(){
         asjfsdkf sadjfklsaçdf asdjfklaçdsf asdjfklçf
       </About>
     </Content>
+
+    <Footer>
+      <Button title="Confirmar" />
+    </Footer>
 
     </Container>
   );
