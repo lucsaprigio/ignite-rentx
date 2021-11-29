f841032206da39b99e88f8c6394004cbce8bdb43
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-11-29 20:48:26 -0300

    Alteraçoes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -9,6 +9,7 @@ import { SchedulingDetails } from '../screens/SchedulingDetails'
 import { SchedulingComplete } from '../screens/SchedulingComplete'
 import { MyCars } from '../screens/MyCars'
 import { SignIn } from '../screens/SignIn'
+import { SignUpFirstStep } from '../screens/SignUp/SignUpFirstStep';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -18,6 +19,10 @@ export function StackRoutes(){
             <Screen 
                 name="SignIn"
                 component={SignIn}
+            />
+            <Screen 
+                name="SignUpFirstStep"
+                component={SignUpFirstStep}
             />
             <Screen 
                 name="Home"
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,89 +1,115 @@
-import React, { useState } from 'react';
-import { 
+import React, { useState } from "react";
+import { useNavigation } from '@react-navigation/native';
+import {
   StatusBar,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
-} from 'react-native';
+  Alert,
+} from "react-native";
+import * as Yup from "yup";
 
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
-import { PasswordInput } from '../../components/PasswordInput';
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
 
-import theme from '../../styles/theme';
+import theme from "../../styles/theme";
 
-import {
-   Container,
-   Header,
-   Title,
-   Form,
-   SubTitle,
-   Footer
-} from './styles';
+import { Container, Header, Title, Form, SubTitle, Footer } from "./styles";
 
-export function SignIn(){
-  const [email, setEmail] =  useState('');
-  const [password, setPassword] =  useState('');
+export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  return(
-    <KeyboardAvoidingView  behavior="position" enabled>
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}> 
-    <Container>
-      <StatusBar  
-        barStyle="dark-content" // Conteúdo da Barra de Status
-        backgroundColor="transparent" // Deixa invisivel a StatusBar
-        translucent 
-      />
-      <Header>
-        <Title>
-          Estamos{'\n'}quase lá.
-        </Title>
-        <SubTitle>
-          Faça seu login para começar{'\n'}
-          uma experiência incrível.
-        </SubTitle>
-      </Header>
+  const navigation = useNavigation();
 
-      <Form>
-        <Input 
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-          onChangeText={setEmail}
-          value={email}
-        />
+  /* Validação */
+  async function handleSignIn() {
+    try{
+      const schema = Yup.object().shape({
+        // Forma da Validação
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string()
+          .required('A senha é obrigatória')
+      });
+  
+      // Passamos o nosso estado acima aqui
+      await schema.validate({ email, password })
 
-        <PasswordInput 
-          iconName="lock"
-          placeholder="Senha"
-          onChangeText={setPassword}
-          value={password}
-        />
-      </Form>
+      //Fazer login.
+    } catch (error){
+      if(error instanceof Yup.ValidationError) {
+       Alert.alert('Opa', error.message);
+      }else {
+         Alert.alert(
+            'Erro na autenticação', 
+            'Ocorreu um erro ao fazer login, verifique as crendenciais'
+          )
+      }
+    }
+  }
 
-      <Footer>
-        <Button 
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
+  function handleNewAccount() {
+    navigation.navigate('SignUpFirstStep');
+  }
 
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle="dark-content" // Conteúdo da Barra de Status
+            backgroundColor="transparent" // Deixa invisivel a StatusBar
+            translucent
+          />
+          <Header>
+            <Title>Estamos{"\n"}quase lá.</Title>
+            <SubTitle>
+              Faça seu login para começar{"\n"}
+              uma experiência incrível.
+            </SubTitle>
+          </Header>
 
+          <Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={setEmail}
+              value={email}
+            />
 
-        <Button 
-          title="Criar conta gratuita"
-          color={theme.colors.background_secondary}
-          light // Tema Booleando criado dentro do componente
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-      </Footer>
-    </Container>
-    </TouchableWithoutFeedback>
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              onChangeText={setPassword}
+              value={password}
+            />
+          </Form>
+
+          <Footer>
+            <Button
+              title="Login"
+              onPress={handleSignIn}
+              enabled={true}
+              loading={false}
+            />
+
+            <Button
+              title="Criar conta gratuita"
+              color={theme.colors.background_secondary}
+              light // Tema Booleando criado dentro do componente
+              onPress={handleNewAccount}
+              enabled={true}
+              loading={false}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 }
